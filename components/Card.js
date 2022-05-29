cef52c266395b63383b4c79da3c20e02abf89279
jorge rafael callejo flores <riskeconomics@gmail.com>
2022-05-29 15:56:49 -0400

    complete card component with tailwindcss

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import { ThumbUpIcon } from '@heroicons/react/outline';
 const Card = ({ res }) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
   return (
-    <section className=''>
+    <section className='p-3 hover:text-white cursor-pointer active:text-red-400 xl:hover:scale-105 transition-transform duration-200'>
       <Image
         layout='responsive'
         src={BASE_URL + res.backdrop_path}
@@ -11,11 +11,13 @@ const Card = ({ res }) => {
         height={100}
       />
       <div className=''>
-        <p className='truncate'>{res.overview}</p>
-        <h2>{res.name || res.original_name || res.original_title}</h2>
-        <p>
+        <p className='truncate text-lg'>{res.overview}</p>
+        <h2 className='text-lg font-bold'>
+          {res.name || res.original_name || res.original_title}
+        </h2>
+        <p className='flex items-center '>
           {res.release_date || res.first_air_date}
-          <ThumbUpIcon className='h-5' />
+          <ThumbUpIcon className='h-5 ml-3 mr-2' />
           {res.vote_count}
         </p>
       </div>
--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const Results = ({ results }) => {
   return (
-    <section>
+    <section className='bg-gray-700 text-gray-200 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  2xl:grid-cols-5 '>
       {results.map((res) => (
         <Card key={res.id} res={res} />
       ))}
