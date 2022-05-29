f08a4724fb3fb329419fe63677d3a510a871c019
jorge rafael callejo flores <riskeconomics@gmail.com>
2022-05-29 12:44:53 -0400

    create navbar components & API from TMDB

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import requests from '../utils/requests';
+import { useRouter } from 'next/router';
+
 const Navbar = () => {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <section className='flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl'>
+      {Object.entries(requests).map(([key, { title, url }]) => {
+        return (
+          <h2
+            onClick={() => router.push(`?genre=${key}`)}
+            key={key}
+            className='m-6 cursor-pointer hover:text-white active:text-red-400'
+          >
+            {title}
+          </h2>
+        );
+      })}
+    </section>
   );
 };
 
